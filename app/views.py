3981c608e71d871e876c7d458606e0450720e351
stephen22otieno <stephenombiro22@gmail.com>
2018-11-22 10:36:26 +0300

    modified:app/views.py

--- app/views.py
+++ app/views.py
@@ -1,13 +1,10 @@
 from flask import render_template
 from app import app
 
-# Views
-@app.route('/')
-def index():
+@app.route('/Blog/<int:Blog_id>')
+def Blog(Blog_id):
 
     '''
-    View root page function that returns the index page and its data
+    View Blog page function that returns the Blog details page and its data
     '''
-
-    message = 'Welcome to My Blog'
-    return render_template('index.html',message = message)
+    return render_template('Blog.html',id = Blog_id)
