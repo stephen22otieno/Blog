e738d1c8390bb89a101d6500118c526c8c980813
stephen22otieno <stephenombiro22@gmail.com>
2018-11-22 10:49:37 +0300

    modified:app/views.py and index.html

--- app/views.py
+++ app/views.py
@@ -7,5 +7,5 @@ def Blog(Blog_id):
     '''
     View Blog page function that returns the Blog details page and its data
     '''
-    title = 'Home - Welcome to The best Blog Review Website Online'
+    title = 'Home - Welcome to The best Blog  Website Online'
     return render_template('index.html', title = title)
