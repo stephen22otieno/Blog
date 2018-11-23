6f447285d26e30de708e90f601c86241ba10e412
stephen22otieno <stephenombiro22@gmail.com>
2018-11-23 14:56:02 +0300

    manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,28 @@
-from app import app
+from flask_script import Manager,Server
+from app import create_app,db
+from app.models import User,Category,Peptalk,Comments
+from flask_migrate import Migrate, MigrateCommand
+
+
+app = create_app('development')
+
+manager =Manager(app)
+migrate = Migrate(app,db)
+manager.add_command('server',Server)
+manager.add_command('db', MigrateCommand)
+app = create_app('test')
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.shell
+def make_shell_contex():
+        return dict(app = app, db = db, Category = Category, User = User, Peptalk = Peptalk, Comments = Comments)
 
 if __name__ == '__main__':
-     app.run()
+    manager.run()
